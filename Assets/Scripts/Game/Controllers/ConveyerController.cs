ece7e20cb26040609fcab664b7dd001999d352f6
HSKKOU <hskk1120551@gmail.com>
2015-10-06 16:19:30 +0900

    change tab size

--- Assets/Scripts/Game/Controllers/ConveyerController.cs
+++ Assets/Scripts/Game/Controllers/ConveyerController.cs
@@ -1,15 +1,18 @@
 using UnityEngine;
 using System.Collections;
 
-public class ConveyerController : SingletonMono<ConveyerController>{
+public class ConveyerController : SingletonMono<ConveyerController>
+{
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+  // Use this for initialization
+  void Start()
+  {
+      
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+
+  }
 }
