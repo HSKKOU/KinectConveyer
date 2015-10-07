ec320d8adaaf94942d91ccbe46ea2f1295f096d7
HSKKOU <hskk1120551@gmail.com>
2015-10-07 14:06:06 +0900

    fix blacket

--- Assets/Scripts/Game/Controllers/ConveyerController.cs
+++ Assets/Scripts/Game/Controllers/ConveyerController.cs
@@ -1,18 +1,15 @@
 using UnityEngine;
 using System.Collections;
 
-public class ConveyerController : SingletonMono<ConveyerController>
-{
+public class ConveyerController : SingletonMono<ConveyerController> {
 
   // Use this for initialization
-  void Start()
-  {
+  void Start() {
       
   }
 
   // Update is called once per frame
-  void Update()
-  {
+  void Update() {
 
   }
 }
