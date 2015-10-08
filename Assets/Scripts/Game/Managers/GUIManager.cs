f0c1d4cf60abf479134022a268ee327a657d6fe7
HSKKOU <hskk1120551@gmail.com>
2015-10-08 13:50:03 +0900

    create GUI

---
+++ Assets/Scripts/Game/Managers/GUIManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GUIManager : SingletonMono<GUIManager> {
+
+  public GameObject GUI;
+  public GameObject TitlePanel;
+
+  public void Initialize() {
+    TitlePanel.SetActive(true);
+  }
+
+  public void HideTitle() {
+    TitlePanel.SetActive(false);
+  }
+}
---
+++ Assets/Scripts/Game/Util/ButtonHandler.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonHandler : MonoBehaviour {
+  
+  public void OnClick() {
+    GameManager.Instance.GameStart();
+  }
+}
